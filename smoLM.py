dda9ddf9c8e890a3d389ff6b7190ed584e9e7ede
Ari Blondal <ari.blondal@gmail.com>
2025-12-15 17:45:50 -0500

    Added comments explaining the smoLM library

--- smoLM.py
+++ smoLM.py
@@ -23,6 +23,7 @@ max_depth = 4
 # LMs are randomized decision trees
 # A state is simply a string of tokens
 class LM:
+	# Initialize a random LM with the given seed
 	def __init__(self, tokenset, depth, seed):
 		self.tokenset = tokenset
 		# print(self.tokenset)
@@ -34,24 +35,24 @@ class LM:
 		}
 		self.fix_probs()
 
+	# Probabilities are set randomly, must be normalized to sum to 1
 	def fix_probs(self, curr=[]):
 		self.layers[len(curr)][*curr] /= np.sum(self.layers[len(curr)][*curr])
 		if len(curr) < max_depth-1:
 			for i in range(len(self.tokenset)-1): # Excluding EOF
 				self.fix_probs(curr + [i])
 
+	# Samples a next token from the LM, given the state
 	def sample_one(self, state):
 		ttable = self.layers[len(state)][*state]
-		# ttable /= np.sum(ttable)
-		# print(np.shape(ttable))
 		r = random.random() - ttable[0]
 		i = 0
 		while(r > 0):
 			i += 1
-			# print(i)
 			r -= ttable[i]
 		return i
 
+	# Samples until EOF (or depth limit)
 	def sample_string(self):
 		state = []
 		while len(state) < self.depth and (len(state) == 0 or state[-1] != EOF):
@@ -60,12 +61,16 @@ class LM:
 			state.append(EOF)
 		return state
 
+	# Given a string, calculates the probability that the LM
+	# 	would output it
 	def prob(self, s):
 		tot = 1
 		for i in range(min(len(s)-1, self.depth)):
 			tot *= self.layers[i][*s[:i+1]]
 		return tot
 
+# Calculates the KL divergence between two LMs
+# Rigorous and exact
 def calc_KL(p: LM, q: LM, depth=max_depth):
 	def recur(curr=[], p_prob=1, q_prob=1):
 		if len(curr) == depth or (len(curr) > 0 and curr[-1] == EOF):
@@ -81,6 +86,9 @@ def calc_KL(p: LM, q: LM, depth=max_depth):
 		return total
 	return recur()
 
+# Calculates the variance of the Monte Carlo estimation method
+# = sum_x (p(x) * [(log p(x) / q(x)) - mu]^2)
+# where mu is the actual KL divergence
 def calc_MC_var(p: LM, q: LM, mu, depth=max_depth):
 	def recur(curr=[], p_prob=1, q_prob=1):
 		if len(curr) == depth or (len(curr) > 0 and curr[-1] == EOF):
