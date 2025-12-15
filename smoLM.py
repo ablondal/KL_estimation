1e6fd274390866f0167618dec8eb482b8504c676
Ari Blondal <ari.blondal@gmail.com>
2025-12-15 17:30:25 -0500

    Added KL divergence calculation, MC sampling, and MC Variance calculations.

--- smoLM.py
+++ smoLM.py
@@ -13,26 +13,12 @@ tokens = [chr(ord('a') + a) for a in range(26)]\
 	+ [chr(ord('0') + a) for a in range(10)]\
 	+ ['.'] # Functions as EOF
 
+def to_string(arr):
+	return ''.join([tokens[a] for a in arr])
+
 EOF = len(tokens)-1
 
 max_depth = 4
-
-def normalize(layers, curr):
-	layers[len(curr)][*curr] /= np.sum(layers[len(curr)][*curr])
-
-def random_LM(tokens, depth):
-	# Transition tables
-	layers = {
-		i: np.random.rand((len(tokenset),)*(i+1))
-		for i in range(depth)
-	}
-
-	layers = {0: tt}
-
-	for t in tokens:
-		lm = fully_random_LM(tokens, depth-1)
-
-
 
 # LMs are randomized decision trees
 # A state is simply a string of tokens
@@ -46,10 +32,17 @@ class LM:
 			i: np.random.rand( *(len(tokenset) for _ in range(i+1)))
 			for i in range(depth)
 		}
+		self.fix_probs()
+
+	def fix_probs(self, curr=[]):
+		self.layers[len(curr)][*curr] /= np.sum(self.layers[len(curr)][*curr])
+		if len(curr) < max_depth-1:
+			for i in range(len(self.tokenset)-1): # Excluding EOF
+				self.fix_probs(curr + [i])
 
 	def sample_one(self, state):
 		ttable = self.layers[len(state)][*state]
-		ttable /= np.sum(ttable)
+		# ttable /= np.sum(ttable)
 		# print(np.shape(ttable))
 		r = random.random() - ttable[0]
 		i = 0
@@ -66,3 +59,39 @@ class LM:
 		if state[-1] != EOF:
 			state.append(EOF)
 		return state
+
+	def prob(self, s):
+		tot = 1
+		for i in range(min(len(s)-1, self.depth)):
+			tot *= self.layers[i][*s[:i+1]]
+		return tot
+
+def calc_KL(p: LM, q: LM, depth=max_depth):
+	def recur(curr=[], p_prob=1, q_prob=1):
+		if len(curr) == depth or (len(curr) > 0 and curr[-1] == EOF):
+			if q_prob == 0:
+				return float('inf')
+			return p_prob * (math.log2(p_prob / q_prob))
+		total = 0
+
+		ptable = p.layers[len(curr)][*curr]
+		qtable = q.layers[len(curr)][*curr]
+		for i in range(len(tokens)):
+			total += recur(curr + [i], p_prob*ptable[i], q_prob*qtable[i])
+		return total
+	return recur()
+
+def calc_MC_var(p: LM, q: LM, mu, depth=max_depth):
+	def recur(curr=[], p_prob=1, q_prob=1):
+		if len(curr) == depth or (len(curr) > 0 and curr[-1] == EOF):
+			if q_prob == 0:
+				return float('inf')
+			return p_prob * (math.log2(p_prob / q_prob) - mu)**2
+		total = 0
+
+		ptable = p.layers[len(curr)][*curr]
+		qtable = q.layers[len(curr)][*curr]
+		for i in range(len(tokens)):
+			total += recur(curr + [i], p_prob*ptable[i], q_prob*qtable[i])
+		return total
+	return recur()
--- test_LMs.py
+++ test_LMs.py
@@ -1,8 +1,32 @@
 from smoLM import *
 
 A = LM(tokens, max_depth, seed=123456)
+B = LM(tokens, max_depth, seed=654321)
 
 # print(A.layers)
 
-for _ in range(15):
-	print(A.sample_string())
+for _ in range(5):
+	print(to_string(A.sample_string()))
+print()
+for _ in range(5):
+	print(to_string(B.sample_string()))
+
+def MC_KL(A, B, n_reps=100000):
+	print(f"Doing Monte Carlo for {n_reps}")
+	tot = 0
+	for i in range(n_reps):
+		sample = A.sample_string()
+		p = A.prob(sample)
+		q = B.prob(sample)
+		if (q == 0):
+			tot += float('inf')
+		else:
+			tot += math.log2(p / q)
+		print(f"so far:{tot / (i+1)}\t\t", end='\r')
+	print(f"Final estimation: {tot / n_reps}")
+
+KL_AB = calc_KL(A, B)
+print(f"KL Divergence between A and B is {KL_AB}")
+MC_KL(A, B, 100)
+Var_KL_AB = calc_MC_var(A, B, KL_AB)
+print(f"The Variance of Monte Carlo is {Var_KL_AB}")
