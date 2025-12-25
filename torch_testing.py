4afaeb7cef10d38566c561c0b252375451bad416
Ari Blondal <ari.blondal@gmail.com>
2025-12-25 13:21:58 -0800

    Test2 folder updated

--- torch_testing.py
+++ torch_testing.py
@@ -84,18 +84,17 @@ def sample_once(
             logits_a = out_a.logits[0, -1] / temperature
             logits_b = out_b.logits[0, -1] / temperature
 
-            p = torch.softmax(logits_a, dim=-1)
             l_p = torch.log_softmax(logits_a, dim=-1)
             l_q = torch.log_softmax(logits_b, dim=-1)
+            p = torch.exp(l_p)
 
             # --- sampling step (modifiable) ---
             probs = next_token_weight(p, l_p, l_q, log_p, log_q)
-
             next_token = torch.multinomial(probs, num_samples=1)
 
-            # print(probs[next_token])
-
-            log_r += torch.log(probs)[next_token].item()
+            # For numerical stability, we really want to use Python floats
+            # as early as possible
+            log_r += math.log(probs[next_token].item())
             log_p += l_p[next_token].item()
             log_q += l_q[next_token].item()
 
@@ -105,6 +104,9 @@ def sample_once(
             break
 
     return tokenizer.decode(input_ids[0]), log_r, log_p, log_q
+
+def just_p(p, l_p, l_q, lg_p, lg_q):
+    return p
 
 def eps_01(p, l_p, l_q, lg_p, lg_q):
     prop = p*torch.abs(l_p - l_q + lg_p - lg_q)
@@ -126,71 +128,107 @@ def eps_09(p, l_p, l_q, lg_p, lg_q):
     prop = 0.1*torch.nn.functional.normalize(prop, p=1, dim=0) + 0.9*p
     return torch.nn.functional.normalize(prop, p=1, dim=0)
 
-kls = []
-kl_sum = 0
-kl_num = 0
-for k in range(100):
-    text, kl_expectations = calc_kl_RB(
-        "An interesting fact is that",
-        max_new_tokens=30,
-        temperature=0.4
-    )
-    kl_sum += sum(kl_expectations)
-    kl_num += 1
-    kls.append(kl_expectations)
-    print(f"KL #{k}: {kl_sum / kl_num}\t\t", end='\r')
-mu = kl_sum / kl_num
-print(f"KL estimate: {mu}\t\t")
-var_sum = 0
-for kl in kls:
-    var_sum += (mu-sum(kl) )*(mu-sum(kl) )
-print(f"Var: {var_sum / kl_num}\t\t")
+def balanced(p, l_p, l_q, lg_p, lg_q):
+    # prop to sqrt(P * |log(P/Q)| * Q / (P + Q))
+    # The epsilon is required so we don't get NaN / infs
+    q = torch.exp(l_q)
+    prop = torch.sqrt((p * q * torch.abs(l_p - l_q + lg_p - lg_q)) / (p + q + 1e-5))
+    return torch.nn.functional.normalize(prop, p=1, dim=0)
 
-with open(sys.argv[2], 'w') as f:
-    json.dump(kls, f, indent=2)
+def adaptive(p, l_p, l_q, lg_p, lg_q):
+    # prop to P * (1 + |log(P/Q)|)
+    prop = p * (1 + torch.abs(l_p - l_q + lg_p - lg_q))
+    return torch.nn.functional.normalize(prop, p=1, dim=0)
 
-# next_token_func = {
-#     0: (lambda p, l_p, l_q, lg_p, lg_q: p),
-#     1: eps_01,
-#     2: eps_05,
-#     3: eps_09
-# }[int(sys.argv[1])]
+def mix09(p, l_p, l_q, lg_p, lg_q):
+    # prop to r = λP + (1-λ)Q for lambda 0.9
+    prop = 0.9 * p + 0.1 * torch.exp(l_q)
+    return torch.nn.functional.normalize(prop, p=1, dim=0)
 
-# particles = []
+def mix03(p, l_p, l_q, lg_p, lg_q):
+    # prop to r = λP + (1-λ)Q for lambda 0.3
+    prop = 0.3 * p + 0.7 * torch.exp(l_q)
+    return torch.nn.functional.normalize(prop, p=1, dim=0)
+
+def balanced2(p, l_p, l_q, lg_p, lg_q):
+    # prop to r = sqrt(P * Q * (log_ratio**2 + 1))
+    q = torch.exp(l_q)
+    prop = torch.sqrt(p * q * (torch.pow(torch.abs(l_p - l_q + lg_p - lg_q), 2) + 1))
+    return torch.nn.functional.normalize(prop, p=1, dim=0)
+
+def geometric(p, l_p, l_q, lg_p, lg_q):
+    q = torch.exp(l_q)
+    # prop to P^beta * Q^(1-beta) with beta = 0.3
+    prop = torch.pow(p, 0.3) * torch.pow(q, 0.7)
+    return torch.nn.functional.normalize(prop, p=1, dim=0)
+
+
+# kls = []
 # kl_sum = 0
-# w_sum = 0
-# for k in range(1000):
-#     text, lg_r, lg_p, lg_q = sample_once(
+# kl_num = 0
+# for k in range(10):
+#     text, kl_expectations = calc_kl_RB(
 #         "An interesting fact is that",
 #         max_new_tokens=30,
-#         temperature=0.4,
-#         next_token_weight = next_token_func
+#         temperature=0.4
 #     )
-#     weight = math.exp(lg_p - lg_r)
-
-#     particles.append(
-#         {'text': text,
-#         'weight': weight,
-#         'val': lg_p - lg_q})
-
-#     # print(text)
-#     # print(f"The weight is {weight:0.6f}")
-#     # print(f"Value of lg_p - lg_q is {lg_p - lg_q}")
-#     kl_sum += weight * (lg_p - lg_q)
-#     w_sum += weight
-#     print(f"KL: {kl_sum / w_sum}\t\t", end='\r')
+#     kl_sum += sum(kl_expectations)
+#     kl_num += 1
+#     kls.append(kl_expectations)
+#     print(f"KL #{k}: {kl_sum / kl_num}\t\t", end='\r')
+# mu = kl_sum / kl_num
+# print(f"KL estimate: {mu}\t\t")
+# var_sum = 0
+# for kl in kls:
+#     var_sum += (mu-sum(kl) )*(mu-sum(kl) )
+# print(f"Var: {var_sum / kl_num}\t\t")
 
 # with open(sys.argv[2], 'w') as f:
-#     json.dump(particles, f, indent=2)
+#     json.dump(kls, f, indent=2)
 
-# kl = kl_sum / w_sum
-# print(f"KL estimate: {kl:0.6f}")
-# # calc variance
-# var_sum = 0
-# for p in particles:
-#     var_sum += p['weight'] * (p['val'] - kl) * (p['val'] - kl)
-# var = var_sum / w_sum
-# print(f"KL var: {var:0.6f}")
+def run_smc(func, length, reps, outp):
+    print(f"Proposal distribution: {outp}\nReps, Length: {reps}, {length}")
+
+    particles = []
+    kl_sum = 0
+    w_sum = 0
+    for k in range(reps):
+        text, lg_r, lg_p, lg_q = sample_once(
+            "An interesting fact is that",
+            max_new_tokens=length,
+            temperature=0.4,
+            next_token_weight = func
+        )
+        weight = math.exp(lg_p - lg_r)
+
+        particles.append(
+            {'text': text,
+            'weight': weight,
+            'val': lg_p - lg_q})
+
+        # print(text)
+        # print(f"The weight is {weight:0.6f}")
+        # print(f"Value of lg_p - lg_q is {lg_p - lg_q}")
+        kl_sum += weight * (lg_p - lg_q)
+        w_sum += weight
+        print(f"KL: {kl_sum / w_sum}\t\t", end='\r')
+
+    with open('tests2/'+outp+'.json', 'w') as f:
+        json.dump(particles, f, indent=2)
+
+    kl = kl_sum / w_sum
+    print(f"KL estimate: {kl:0.6f}")
+    # calc variance
+    var_sum = 0
+    for p in particles:
+        var_sum += p['weight'] * (p['val'] - kl) * (p['val'] - kl)
+    var = var_sum / w_sum
+    print(f"KL var: {var:0.6f}")
+
+n_reps = 1000
+t_length = 20
+for ff in sys.argv[1:]:
+    run_smc(eval(ff), t_length, n_reps, ff)
 
 
 # print("Mean KL:", sum(kl_per_token) / len(kl_per_token))
